ce5db34529f4d969a9f963cd70dd03be7fa7b2aa
Soham Kolhatkar <sohamk130405@gmail.com>
2025-02-27 13:36:55 +0530

    added marks functionality to submission

--- features/submissions/components/submissions-table.tsx
+++ features/submissions/components/submissions-table.tsx
@@ -10,7 +10,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Doc } from "@/convex/_generated/dataModel";
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import Link from "next/link";
+import { useUpdateSubmission } from "../api/use-update-submission";
 
 interface SubmissionsProp {
   submissions:
@@ -22,6 +24,25 @@ interface SubmissionsProp {
 
 const SubmissionsTable = ({ submissions }: SubmissionsProp) => {
   const [editingId, setEditingId] = useState("");
+  const [editedMarks, setEditedMarks] = useState<number | "">("");
+  const { mutate: updateMarks, isPending } = useUpdateSubmission();
+  // Handle input change
+  const handleMarksChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedMarks(Number(e.target.value));
+  };
+
+  // Cancel editing
+  const handleCancel = () => {
+    setEditingId("");
+    setEditedMarks("");
+  };
+
+  // Submit edited marks
+  const handleSubmit = async (id: Id<"submissions">) => {
+    updateMarks({ marks: editedMarks as number, submissionId: id });
+    handleCancel();
+  };
+
   return (
     <>
       <h2 className="text-xl mb-4">Submissions</h2>
@@ -38,53 +59,72 @@ const SubmissionsTable = ({ submissions }: SubmissionsProp) => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {submissions.map((submission) => (
-              <TableRow key={submission._id}>
-                <TableCell>{submission.user?.name}</TableCell>
-                <TableCell>{submission.user?.rollNo}</TableCell>
-                <TableCell>{submission.user?.prn}</TableCell>
-                <TableCell>
-                  <a
-                    href={submission.fileUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FileText />
-                  </a>
-                </TableCell>
-                <TableCell>
-                  {editingId === submission._id ? (
-                    <Input
-                      type="number"
-                      value={submission.marks}
-                      className="w-[80px]"
-                    />
-                  ) : (
-                    submission.marks
-                  )}
-                </TableCell>
-                <TableCell>
-                  {editingId === submission._id ? (
-                    <div className="flex gap-2">
-                      <Button variant="outline" size="sm" className="ml-2">
-                        <Check />
-                      </Button>
-                      <Button variant="outline" size="sm" className="ml-2">
-                        <X />
-                      </Button>
-                    </div>
-                  ) : (
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      onClick={() => setEditingId(submission._id)}
+            {submissions
+              .sort((a, b) => (a.user?.rollNo ?? 0) - (b.user?.rollNo ?? 0))
+              .map((submission) => (
+                <TableRow key={submission._id}>
+                  <TableCell>{submission.user?.name}</TableCell>
+                  <TableCell>{submission.user?.rollNo}</TableCell>
+                  <TableCell>{submission.user?.prn}</TableCell>
+                  <TableCell>
+                    <Link
+                      href={submission.fileUrl as string}
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <Pencil className="w-4 h-4" />
-                    </Button>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))}
+                      <FileText />
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    {editingId === submission._id ? (
+                      <Input
+                        type="number"
+                        value={editedMarks}
+                        onChange={handleMarksChange}
+                        className="w-[80px]"
+                      />
+                    ) : (
+                      submission.marks
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {editingId === submission._id ? (
+                      <div className="flex gap-2">
+                        <Button
+                          disabled={isPending}
+                          variant="outline"
+                          size="sm"
+                          className="ml-2"
+                          onClick={() => handleSubmit(submission._id)}
+                        >
+                          <Check />
+                        </Button>
+                        <Button
+                          disabled={isPending}
+                          variant="outline"
+                          size="sm"
+                          className="ml-2"
+                          onClick={handleCancel}
+                        >
+                          <X />
+                        </Button>
+                      </div>
+                    ) : (
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        size="icon"
+                        onClick={() => {
+                          setEditingId(submission._id);
+                          setEditedMarks(submission.marks || "");
+                        }}
+                      >
+                        <Pencil className="w-4 h-4" />
+                      </Button>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       ) : (
