b3406005ff4dea8c29e59054c40cca1eaf62f186
Soham Kolhatkar <sohamk130405@gmail.com>
2025-02-27 14:56:15 +0530

    added domain for external links

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["enduring-finch-134.convex.cloud"],
+    domains: [
+      "enduring-finch-134.convex.cloud",
+      "wonderful-dolphin-404.convex.cloud",
+    ],
   },
 };
 
